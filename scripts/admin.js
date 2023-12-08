c5bd52de76662a44b965cf66037dc3412d43d262
DarrenViljoen32 <31759385+DarrenViljoen32@users.noreply.github.com>
2023-12-08 12:46:13 +0200

    made more changes.

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,7 +1,18 @@
-let products = JSON.parse(localStorage.getItem('products')) ||[];
+//parse the product data from local storage
+//make it an empty array if theres no data
+let products;
+try{
+    products = JSON.parse(localStorage.getItem('products')) ||[];
+}
+catch (error){
+    console.error(`There has been an Error in parsing JSON from local storage.`);
+    products = [];
+} 
 
+//when there is no products in the array create a constructor function for objects and store it in the products array
 if(!products.length){
 
+    //constructor function to make objects
     function MyObjectConstructor(id, name, description, price, prodImage, amount){
         this.id = id;
         this.name = name;
@@ -11,6 +22,7 @@ if(!products.length){
         this.amount = amount;
     }
 
+    //objects with id, name, description, price, url, and amount
     let product01 = new MyObjectConstructor(1, `Grand Theft Auto V`, `Grand Theft Auto V is a 2013 action-adventure game set within the fictional state of San Andreas, the single-player story follows three protagonists and their attempts to commit heists while under pressure from a corrupt government agency and powerful criminals.`, 700, `https://i.postimg.cc/nr2VFz60/Grand-Theft-Auto-V.png`, 0);
 
     let product02 = new MyObjectConstructor(2, `Red Dead Redemption 2`, `Red Dead Redemption 2 is a western action-adventure game set in an open world environment and played from a third-person perspective, featuring single-player and online multiplayer components. Relive the final years of the Outlaw in America's Wild West era comes to an end.`, 800, `https://i.postimg.cc/NFyR33yJ/reddead2.jpg`, 0);
@@ -23,38 +35,56 @@ if(!products.length){
 
     let product06 = new MyObjectConstructor(6, `Assassin's Creed Mirage`, `Assassin's Creed Mirage is a 2023 action-adventure game developed by Ubisoft Bordeaux and published by Ubisoft. The game is the thirteenth major installment in the Assassin's Creed series and the successor to 2020's Assassin's Creed Valhalla.`, 1200, `https://i.postimg.cc/SQn5hy4L/Assassin-s-Creed-Mirage-cover.jpg`, 0);
 
+    //push the objects into the array
     products.push(product01, product02, product03, product04, product05, product06);
 
+    //save the array to the local storage
     localStorage.setItem(`products`, JSON.stringify(products));
 }
 
+//fetch the array from local storage
 products = JSON.parse(localStorage.getItem(`products`));
 
+//set a variable for the html table
 let myTable = document.querySelector(`table`);
 
+//function to create products in the html table
 function display(){
     if(products.length === 0){
+        //spinner if theres no products
         myTable.innerHTML = `
-        <div id="spinner" class="spinner-border text-primary" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
+            <div id="spinner" class="spinner-border text-primary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
         `
-    }else{
+    }
+    else{
+        //make html for every product then update the table
         let items = products.map(function(product, index){
+            let imageElement;
+            try{
+                imageElement = `<img src="${product.prodImage}">`;
+            }
+            catch (error){
+                console.error(`There has been an Error loading the Image: `, error);
+                imageElement = `Image is not Available.`
+            }
+
             return `
-                <tr>
+                <tr id='tableHead'>
                     <th>Id.</th>
                     <th>Name</th>
                     <th>Description</th>
                     <th>Price</th>
                     <th>Image</th>
+                    <th></th>
                 </tr>
-                <tr>
+                <tr class='table-active'>
                     <td>${product.id}:</td>
                     <td>${product.name}</td>
                     <td>${product.description}</td>
                     <td>R${product.price}</td>
-                    <td><img src="${product.prodImage}"></td>
+                    <td>${imageElement}</td>
                     <td><button class='editBtn' value= '${index}'>Edit</button>
                     <br><br>
                     <button class = 'delete' value = '${index}'>Delete</button></td>
@@ -65,14 +95,13 @@ function display(){
     }
 }
 
-
+//initialize the array and display the products
 setArray();
-
 display();
 
+//function to set the array and then update local storage
 function setArray(){
     localStorage.setItem(`products`, JSON.stringify(products));
-
     products = JSON.parse(localStorage.getItem(`products`));
 }
 
@@ -80,24 +109,27 @@ function setArray(){
 let sortBtn = document.querySelector('#adminSort');
 let j = false;
 sortBtn.addEventListener("click", ()=>{
+    //sort products in descending order
     if(!j){
         products.sort((a, b) => b.price - a.price);
-        sortBtn.textContent = "Sorted by highest price";
+        sortBtn.textContent = "Sorted in Descending Order";
         j = true;
-    }else{
+    }
+    //sort products in ascending order
+    else{
         products.sort((a, b) => a.price - b.price);
-        sortBtn.textContent = "Sorted by lowest price";
+        sortBtn.textContent = "Sorted by Ascending Order";
         j = false;
     }
     display(products);
-});
+})
 
-
+//edit button and model
 let editProductButton = document.querySelector(`.editBtn`);
 let editModal = document.querySelector(`#editModal`);
 let editBtn = document.querySelector(`#editConfirmBtn`);
 
-// Element that closes the modal
+//element that closes the modal
 let editSpan = document.getElementsByClassName("editClose")[0];
 editSpan.onclick = function(){
     editModal.style.display = "none";
@@ -105,16 +137,15 @@ editSpan.onclick = function(){
 
 //addEventListener for edit button
 myTable.addEventListener(`click`, function(){
-    if(event.target.classList.contains(`editBtn`)){
-        openEditModal(event.target.value);
+    try{
+        if(event.target.classList.contains(`editBtn`)){
+            openEditModal(event.target.value);
+        }
     }
-});
-
-//Alternate event listener
-// editProductButton.onclick = function(){
-//     editModal.style.display = "block";
-//     openEditModal(event.target.value);
-// }
+    catch(error){
+        console.error(`There has been an Error in the handling click event: `, error);
+    }
+})
 
 //function to open modal when edit button is clicked
 function openEditModal(index){
@@ -131,6 +162,7 @@ function openEditModal(index){
     };
     editModal.style.display = "block";
 }
+
 //function to edit product values
 function editProduct(index){
     let editNameValue = document.querySelector(`#editName`).value;
@@ -140,6 +172,7 @@ function editProduct(index){
     let editValid = document.querySelector(`#editValidation`);
     editValid.innerHTML = '';
     
+    //check if values are empty
     if(editNameValue.trim() !== '' && editDescValue.trim() !== '' && editPriceValue.trim() !== '' && editImgURL.trim() !== ''){
         //update product info
         products[index].name = editNameValue;
@@ -149,7 +182,8 @@ function editProduct(index){
 
         setArray();
         display();
-    }else{
+    }
+    else{
         editValid.innerHTML = 'Please make sure to enter values in every field.';
     }
 }
@@ -163,94 +197,96 @@ function remove(place){
     display();
 }
 
+//event listener for the delete button
 myTable.addEventListener(`click`, function(){
     if(event.target.classList.contains(`delete`)){
         remove(event.target.value);
     }
 })
 
-// Modal
+//modal
 let modal = document.getElementById("myModal");
-// Button that opens the modal
+
+//button that will open the modal
 let btn = document.getElementById("myBtn");
-// Element that closes the modal
+
+//element that will close the modal
 let span = document.getElementsByClassName("close")[0];
-// When button is clicked it will open the modal 
+
+//when button is clicked it will open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-// When the user clicks on X it will close the modal
+
+//when the user clicks on X it will close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-// When the user clicks anywhere outside of the modal it will close it
+
+//when the user clicks anywhere outside of the modal it will close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-//confirm button
+//counter for making unique Ids for new products
 let idCounter = products.length > 0 ? Math.max(...products.map(item => item.id)) + 1 : 0;
 
-let confirmButton = document.querySelector(`#confirmBtn`)
+//set variable for confirm button
+let confirmButton = document.querySelector(`#confirmBtn`);
+
+//event listener for the confirm button
 confirmButton.addEventListener(`click`, function(){
-    let nameValue = document.querySelector(`#prodName`).value;
-    let descValue = document.querySelector(`#prodDesc`).value;
-    let priceValue = document.querySelector(`#prodPrice`).value;
-    let imgUrl = document.querySelector(`#prodURL`).value;
-    let checkValid = document.querySelector(`#validation`);
-    checkValid.innerHTML = ``;
-
-    let obj = {
-        id : products.id,
-        name : nameValue,
-        description : descValue,
-        price : priceValue,
-        prodImage : imgUrl,
-        amount : products.amount,
-    }
-
-    if(nameValue.trim() !== `` && descValue.trim() !== `` && priceValue.trim() !== `` && imgUrl.trim() !== ``){
-        obj.id = idCounter++;
-        products.push(obj); 
-        localStorage.setItem(`products`, JSON.stringify(products));
-        display();
+    try{
+        //get the values from the input fields
+        let nameValue = document.querySelector(`#prodName`).value;
+        let descValue = document.querySelector(`#prodDesc`).value;
+        let priceValue = document.querySelector(`#prodPrice`).value;
+        let imgUrl = document.querySelector(`#prodURL`).value;
         
+        //set variable to the validation element
+        let checkValid = document.querySelector(`#validation`);
+        checkValid.innerHTML = ``;
 
-        document.querySelector(`#prodName`).value = ``;
-        document.querySelector(`#prodDesc`).value = ``;
-        document.querySelector(`#prodPrice`).value = ``;
-        document.querySelector(`#prodURL`).value = ``;
-        console.log(products);
+        //make an object for the new product
+        let obj = {
+            id : products.id,
+            //id: obj.id,
+            name : nameValue,
+            description : descValue,
+            price : priceValue,
+            prodImage : imgUrl,
+            amount : products.amount,
+            //amount: obj.amount,
+        }
+
+        //check if the input fields are filled
+        if(nameValue.trim() !== `` && descValue.trim() !== `` && priceValue.trim() !== `` && imgUrl.trim() !== ``){
+            //make a unique Id and assign it to the new product
+            obj.id = idCounter++;
+
+            //add the new product to the array
+            products.push(obj); 
+
+            //save the updated array to local storage
+            localStorage.setItem(`products`, JSON.stringify(products));
+            display();
+            
+            //clear all input fields after adding the product
+            document.querySelector(`#prodName`).value = ``;
+            document.querySelector(`#prodDesc`).value = ``;
+            document.querySelector(`#prodPrice`).value = ``;
+            document.querySelector(`#prodURL`).value = ``;
+
+            console.log(products);
+        }
+        else{
+            //display validation error if fields are empty
+            checkValid.innerHTML = `Please make sure to enter all fields.`;
+        }
     }
-    else{
-        checkValid.innerHTML = `Please make sure to enter all fields.`;
+    catch(error){
+        console.error(`There has been an Error validating or creating the Product: `, error);
     }
 })
-
-// //edit button
-// let editBtn = document.querySelector(`#editBtn`);
-// //edit modal
-// let editModal = document.querySelector(`#editModal`);
-// //close modal
-// let editSpan = document.getElementsByClassName("editClose")[0];
-// //when button is clicked open the modal
-// editBtn.onclick = function(){
-//     editModal.style.display = "block";
-// }
-// // When the user clicks on X it will close the modal
-// editSpan.onclick = function() {
-//     editModal.style.display = "none";
-//   }
-
-// //display product values in input fields
-// document.querySelector(`#editName`);
-// document.querySelector(`#editDesc`);
-// document.querySelector(`#editPrice`);
-// document.querySelector(`#editURL`);
-
-// let editConfirmButton = document.querySelector(`#editConfirmBtn`);
-// editConfirmButton.addEventListener(`click`, function(){
-
-// })
